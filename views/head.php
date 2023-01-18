f5c1016ec5ec163075a31392d06e7dd68b64b3f0
david reinfurt <reinfurt@o-r-g.com>
2023-01-18 11:20:38 -0500

    add mobile-app tags to achieve translucent status bar on mobile in process

--- views/head.php
+++ views/head.php
@@ -35,6 +35,9 @@ if($uu->id) {
 		<title><? echo $site; ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- <meta name="theme-color" content="#000"> -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		<link rel="stylesheet" href="/static/css/main.css">
 		<link rel="stylesheet" href="/static/css/sf-text.css">
 		<link rel="stylesheet" href="/static/css/sf-mono.css">
