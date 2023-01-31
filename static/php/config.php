d7e934ff69771c3d08a8a43005904d9df30a91d5
wei <wei@o-r-g.com>
2023-01-31 10:13:08 +0800

    update config.php

--- static/php/config.php
+++ static/php/config.php
@@ -6,4 +6,8 @@
 $head = 'VIS 415';
 $site = 'R-e-s-e-a-r-c-h';
 $home = $head . ", " . $site;
+$card_default = '/media/jpg/card-default.jpg';
+$description = 'Hello, w-w-w-orld.';
+$site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+$site_url .= '://' . $_SERVER['SERVER_NAME'];
 ?>
